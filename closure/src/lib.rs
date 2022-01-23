11278a7d30b53a2c0d21c31c1fe97f71bc712b1d
kinsho-stb <tomoya.kinsho@stanby.inc>
2022-01-23 16:46:56 +0900

    Feat: closure

---
+++ closure/src/lib.rs
@@ -0,0 +1,83 @@
+pub mod test {
+
+    pub fn main2() {
+        {
+            // closureに型情報を付与する必要はないが、明示的に型注釈を付与することもできる
+            let function = |num: u32| -> u32 {
+                num
+            };
+
+            // ↓と一緒
+            let function2 = |num: u32| {
+                num
+            };
+        }
+
+
+        {
+
+            // 全部同じ定義！！
+            fn function(num: u32) -> u32 {
+                num + 1
+            }
+            let function2 = |num: u32| -> u32 {
+                num + 1
+            };
+
+            let function3 = |num: u32| {
+                num + 1
+            };
+
+            let function4 = |num: u32| num + 1;
+
+        }
+
+        // この場合はエラーでる
+        {
+
+            let function = |num| num;
+
+            let res1 = function(String::from("kinsho"));
+            // let res2 = function(5); // ↑でclosureの引数型はStringであると推測されたので、u32は引数に指定できない
+        }
+
+        // Fn traitを
+        // 使ってclosureを保持する構造体を作ってみる
+        // このCacherでmain.rsのコードをリファクタリングしてみる
+        {
+            struct Cacher<T> where T: Fn(u32) -> u32{
+                calculation: T,
+                value: Option<u32>
+            }
+
+            impl<T> Cacher<T> where T: Fn(u32) -> u32 {
+                // Cacherを作った時点では、関数だけ保持しておいて、関数の実行結果であるvalueにはNoneを格納する
+                fn new(calculation: T) -> Cacher<T> {
+                    Cacher {
+                        calculation,
+                        value: None
+                    }
+                }
+
+                //
+                fn value(&mut self, arg: u32) -> u32 {
+                    match self.value {
+                        Some(v) => v,
+                        None => {
+                            let v = (self.calculation)(arg);
+                            self.value = Some(v);
+                            v
+                        }
+                    }
+                }
+
+            }
+
+            let func = |num: u32| num * 2;
+            let mut cacher = Cacher::new(func);
+            cacher.value(3);
+
+        }
+
+    }
+}
---
+++ closure/src/main.rs
@@ -0,0 +1,99 @@
+
+// クロージャ・・・変数に突っ込んだり、関数の引数として渡してあげることのできる匿名関数のこと
+
+use std::thread;
+use std::time::Duration;
+use closure::test;
+
+fn main() {
+    let simulated_user_specified_value = 10;
+    let simulated_random_number = 7;
+
+    generate_workout(
+        simulated_user_specified_value,
+        simulated_random_number
+    );
+
+    test::main2();
+    {
+
+    }
+
+
+}
+
+// TODO: Cacherを以下二点でリファクタする
+// 1. 現状どんな値をargに渡されても、1回目に保存した値しか返さないようになっている（valueをhashmapで持たせるようにする）
+// 2.　u32しか受け取らないようになっているので、ジェネリクスを使ってu32以外も受け取れるようにする
+struct Cacher2<T> where T: Fn(u32) -> u32 {
+    calculation: T, // functionを受ける
+    value: Option<u32>
+}
+
+impl Cacher2<T> {
+
+    fn new(calculation: T) -> Cacher2<T> {
+        Cacher2 {
+            calculation,
+            value: None
+        }
+    }
+
+    fn value(&mut self, arg: u32) -> u32 {
+        match self.value {
+            Some(v) => v,
+            None => {
+                let v = (self.calculation)(arg);
+                self.value = Some(v);
+                v
+            }
+        }
+    }
+
+}
+
+
+fn generate_workout(intensity: u32, random_number: u32) {
+    let expensive_closure = |num| {
+        println!("calculate");
+        thread::sleep(Duration::from_secs(2));
+        num
+    };
+
+    let mut cacher2 = Cacher2::new(expensive_closure);
+
+    // これで一回関数が実行されて、valueに値があるならCacherに保存されているvalueが取り出されるだけになった！
+    if intensity < 25 {
+        println!(
+            // 今日は{}回腕立て伏せをしてください！
+            "Today, do {} pushups!",
+            cacher2.value(intensity)
+        );
+
+        println!(
+            // 次に、{}回腹筋をしてください！
+            "Next, do {} situps!",
+            cacher2.value(intensity)
+        );
+    } else {
+        if random_number == 3 {
+            // 今日は休憩してください！水分補給を忘れずに！
+            println!("Take a break today! Remember to stay hydrated!");
+        } else {
+            println!(
+                // 今日は、{}分間走ってください！
+                "Today, run for {} minutes!",
+                cacher2.value(intensity)
+            );
+        }
+    }
+}
+
+
+fn simulated_expensive_calculation(intensity: u32) -> u32 {
+    println!("calculate");
+    thread::sleep(Duration::from_secs(2));
+    intensity
+}
+
+

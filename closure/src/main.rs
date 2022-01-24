bf145cd3365b55e5493c6f3bca7a193284ab3f53
kinsho-stb <tomoya.kinsho@stanby.inc>
2022-01-25 00:50:15 +0900

    Refactor: hashMap

--- closure/src/main.rs
+++ closure/src/main.rs
@@ -1,6 +1,8 @@
 
 // クロージャ・・・変数に突っ込んだり、関数の引数として渡してあげることのできる匿名関数のこと
 
+use std::collections::hash_map::Entry;
+use std::collections::HashMap;
 use std::thread;
 use std::time::Duration;
 use closure::test;
@@ -25,29 +27,40 @@ fn main() {
 // TODO: Cacherを以下二点でリファクタする
 // 1. 現状どんな値をargに渡されても、1回目に保存した値しか返さないようになっている（valueをhashmapで持たせるようにする）
 // 2.　u32しか受け取らないようになっているので、ジェネリクスを使ってu32以外も受け取れるようにする
-struct Cacher2<T> where T: Fn(u32) -> u32 {
+struct Cacher2<'a, T> where T: Fn(u32) -> u32 {
     calculation: T, // functionを受ける
-    value: Option<u32>
+    value: &'a mut HashMap<u32, u32>
 }
 
-impl<T> Cacher2<T> where T: Fn(u32) -> u32{
+impl<'a, T> Cacher2<'a, T> where T: Fn(u32) -> u32{
 
-    fn new(calculation: T) -> Cacher2<T> {
+    fn new(calculation: T, hash_map: &'a mut HashMap<u32, u32>) -> Cacher2<T> {
         Cacher2 {
             calculation,
-            value: None
+            value: hash_map
         }
     }
 
-    fn value(&mut self, arg: u32) -> u32 {
-        match self.value {
-            Some(v) => v,
-            None => {
-                let v = (self.calculation)(arg);
-                self.value = Some(v);
-                v
+    fn get_or_insert(&mut self, arg: u32) -> &mut u32 {
+        // get or insertしたいならEntryを使う
+        match self.value.entry(arg) {
+            Entry::Occupied(o) => o.into_mut(), // map自身と同じライフタイムを持つvalueを返す
+            Entry::Vacant(v) => {
+                let res = (self.calculation)(arg);
+                v.insert(res) // 格納した値のmutableな参照を返す
             }
         }
+        // [メモ]
+        // 同じスコープ内では、mutableの借用は一回しかできない
+        // immutableの借用が起きていても、その後でmutableの借用はできない！
+        // match self.value.get(&arg) {
+        //     Some(v) => v,
+        //     None => {
+        //         &(self.calculation)(arg)
+        //         // self.value.insert(v, v); // これはmutableの借用になるのでできない。。。self.value.getでimmutableの借用を行っているから
+        //     }
+        //
+        // }
     }
 
 }
@@ -60,20 +73,23 @@ fn generate_workout(intensity: u32, random_number: u32) {
         num
     };
 
-    let mut cacher2 = Cacher2::new(expensive_closure);
+    // [メモ]
+    // HashMapは、insertとするとその値の所有権はhashmapにムーブされる！！
+    let mut map: HashMap<u32, u32> = HashMap::new();
+    let mut cacher2 = Cacher2::new(expensive_closure, &mut map);
 
     // これで一回関数が実行されて、valueに値があるならCacherに保存されているvalueが取り出されるだけになった！
     if intensity < 25 {
         println!(
             // 今日は{}回腕立て伏せをしてください！
             "Today, do {} pushups!",
-            cacher2.value(intensity)
+            cacher2.get_or_insert(intensity)
         );
 
         println!(
             // 次に、{}回腹筋をしてください！
             "Next, do {} situps!",
-            cacher2.value(intensity)
+            cacher2.get_or_insert(intensity)
         );
     } else {
         if random_number == 3 {
@@ -83,7 +99,7 @@ fn generate_workout(intensity: u32, random_number: u32) {
             println!(
                 // 今日は、{}分間走ってください！
                 "Today, run for {} minutes!",
-                cacher2.value(intensity)
+                cacher2.get_or_insert(intensity)
             );
         }
     }

91b80f11cad6737d02476e84d3fad5e1983bb17f
kinsho-stb <tomoya.kinsho@stanby.inc>
2022-02-13 14:47:58 +0900

    Feat: unsafe and macro

---
+++ macros/src/main.rs
@@ -0,0 +1,15 @@
+use proc_macro;
+
+fn main() {
+
+
+    {
+
+        #[some_attribute]
+        pub fn some_name(input: TokenStream) -> TokenStream {
+
+        }
+
+    }
+
+}
--- unSafe/src/main.rs
+++ unSafe/src/main.rs
@@ -1,3 +1,20 @@
 fn main() {
-    println!("Hello, world!");
+
+    // 生ポインタ
+    {
+        let mut num = 5;
+        // 生ポインタを作成している;
+        // これらは参照が有効であるとわかる
+        let r1 = &num as *const i32;
+        let r2 = &mut num as *const i32;
+    }
+
+    {
+        // 参照が有効なのかわからない
+        let address = 0x012345i32;
+        let r1 = &address as *const i32;
+    }
+
+    // 使い所がよくわからないので必要になったら学ぶ
+
 }

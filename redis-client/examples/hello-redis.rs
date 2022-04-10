e19ebd36475cbc542e32ec5a551490ccc4edc9e7
kinsho-stb <tomoya.kinsho@stanby.inc>
2022-04-10 18:57:47 +0900

    Feat: redis client

--- redis-client/examples/hello-redis.rs
+++ redis-client/examples/hello-redis.rs
@@ -1,19 +1,64 @@
+use bytes::Bytes;
 use mini_redis::{client, Result};
+use tokio::sync::mpsc::Sender;
+use crate::Command::{Get, Set};
 
 #[tokio::main]
 // このtokio::mainはmacroになっており、ランタイムの初期化と非同期処理の実行が行われる
-//
-pub async fn main() -> Result<()> {
-    // mini-redis アドレスへのコネクションを開く
-    let mut client = client::connect("127.0.0.1:6379").await?;
+pub async fn main() {
+    let (tx, mut rx) = tokio::sync::mpsc::channel(100);
 
-    // "hello" というキーに "world" という値をセット
-    client.set("hello", "world".into()).await?;
+    let set_tx = tx.clone();
 
-    // "hello" の値を取得
-    let result = client.get("hello").await?;
-    //
-    println!("got value from the server; result={:?}", result);
+    let task1 = tokio::spawn(async move {
+        // NOTE: redis serverからの結果を受け取るためにそれ用のチャネルを作成する
+        let (one_tx, one_rx) = tokio::sync::oneshot::channel();
+        // NOTE: menagerタスク側がserverから受け取ったレスポンスをone_txを通して送る
+        tx.send(Command::Get {key: String::from("hello"), sender: one_tx}).await;
+        // NOTE: one_tx（managerタスク）からのメッセージを待つ
+        let res = one_rx.await;
+        println!("GOT {:?}", res);
+    });
 
-    Ok(())
+    let task2 = tokio::spawn(async move {
+        // NOTE: redis serverからの結果を受け取るためにそれ用のチャネルを作成する
+        let (one_tx, one_rx) = tokio::sync::oneshot::channel();
+        // NOTE: menagerタスク側がserverから受け取ったレスポンスをone_txを通して送る
+        set_tx.send(Command::Set {key: String::from("hello"), value: "world".into(), sender: one_tx}).await;
+        // NOTE: one_tx（managerタスク）からのメッセージを待つ
+        let res = one_rx.await;
+        println!("GOT {:?}", res);
+    });
+
+    let manager = tokio::spawn(async move {
+        // mini-redis アドレスへのコネクションを開く
+        let mut client = client::connect("127.0.0.1:6379").await.unwrap();
+        while let Some(cmd) = rx.recv().await {
+            match cmd {
+                Get {key, sender} => {
+                    let res = client.get(&key).await;
+                    let _ = sender.send(res);
+                },
+                Set {key, value, sender} => {
+                    let res = client.set(&key, value.into()).await;
+                    let _ = sender.send(res);
+                }
+            }
+        }
+    });
+    task1.await.unwrap();
+    task2.await.unwrap();
+    manager.await.unwrap();
+}
+
+enum Command {
+    Set {
+        key: String,
+        value: Vec<u8>,
+        sender: tokio::sync::oneshot::Sender<mini_redis::Result<()>>
+    },
+    Get {
+        key: String,
+        sender: tokio::sync::oneshot::Sender<mini_redis::Result<Option<Bytes>>>
+    }
 }

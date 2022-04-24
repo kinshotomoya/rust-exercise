381ec05799ad25dd5c3eb07a24b776538179658f
kinsho-stb <tomoya.kinsho@stanby.inc>
2022-04-24 20:15:24 +0900

    complete mini-tokio

--- mini-tokio/src/main.rs
+++ mini-tokio/src/main.rs
@@ -2,45 +2,142 @@
 // https://zenn.dev/magurotuna/books/tokio-tutorial-ja/viewer/async_in_depth#mini-tokio
 
 use std::collections::VecDeque;
+use std::fmt::Debug;
 use std::future::Future;
 use std::pin::Pin;
-use std::task::Context;
-use futures::task;
+use std::sync::{Arc, Mutex, MutexGuard};
+use std::task::{Context, Poll};
+use std::time::{Duration, Instant};
+use futures::{select, task};
+use futures::task::ArcWake;
+use crossbeam::channel;
 
 fn main() {
+    let mut mini_tokio = MiniTokio::new();
+    mini_tokio.spawn(async {
+
+        let when = Instant::now() + Duration::from_secs(10);
+        let future = Delay { when };
+
+        let out = future.await;
+    });
 
 
+    mini_tokio.spawn(async {
+
+        let when = Instant::now() + Duration::from_secs(12);
+        let future = Delay { when };
+
+        let out = future.await;
+    });
+
+
+    mini_tokio.run();
 
 }
 
-type Task = Pin<Box<dyn Future<Output = ()> + Send>>;
-
 struct MiniTokio {
-    tasks: VecDeque<Task>
+    scheduled: channel::Receiver<Arc<Task>>,
+    sender: channel::Sender<Arc<Task>>
 }
 
 impl MiniTokio {
     fn new() -> MiniTokio {
+        // std::channelはSyncを実装していない（非同期対応）ので、crossbeam crateを利用する
+        let (sender, receiver) = channel::unbounded();
         MiniTokio {
-            tasks: VecDeque::new()
+            scheduled: receiver,
+            sender
         }
     }
 
     // 引数で受けとったfutureをruntimeで共有のqueueにpushしている
     fn spawn<F>(&mut self, future: F)
         where F: Future<Output = ()> + Send + 'static {
-        self.tasks.push_back(Box::pin(future));
+        Task::spawn(future, &self.sender)
     }
 
-    pub fn run(&mut self) {
-        let waker = task::noop_waker();
-        let mut cx = Context::from_waker(&waker);
-
-        while let Some(mut task) = self.tasks.pop_front() {
-            if task.as_mut().poll(&mut cx).is_pending() {
-                self.tasks.push_back(task);
-            }
+    fn run(&mut self) {
+        while let Ok(task) = self.scheduled.recv() {
+            println!("wwwww");
+            task.pool();
         }
+    }
+}
 
+
+
+// taskは紐づくWakerがwake()されたタイミングでチャネルの送信側を使ってtaskを送信する必要がある
+struct Task {
+    future: Mutex<Pin<Box<dyn Future<Output = ()> + Send>>>,
+    executor: channel::Sender<Arc<Task>>
+}
+
+impl Task {
+    fn schedule(self: &Arc<Self>) {
+        self.executor.send(self.clone());
+    }
+
+    fn pool(self: Arc<Self>) {
+        let waker = task::waker(self.clone());
+        let mut cx = Context::from_waker(&waker);
+        let mut future: MutexGuard<Pin<Box<dyn Future<Output=()> + Send>>> = self.future.try_lock().unwrap();
+
+        // futureをポーリングしている（Delay.poolが呼び出されている）
+        future.as_mut().poll(&mut cx);
+    }
+
+    // 受け取ったfutureをTask構造体にラップしてチャネルに送信する
+    // mini-tokio runした時点でこのtaskをreceiveしている
+    fn spawn<F>(future: F, channel: &channel::Sender<Arc<Task>>) where F: Future<Output = ()> + Send + 'static {
+        let task = Arc::new(Task {
+            future: Mutex::new(Box::pin(future)),
+            executor: channel.clone()
+        });
+
+        channel.send(task);
+    }
+}
+
+impl ArcWake for Task {
+    // taskに紐づくwakerがwakeしたらこのメソッドが呼ばれる
+    fn wake_by_ref(arc_self: &Arc<Self>) {
+        println!("mutt");
+        arc_self.schedule();
+    }
+}
+
+struct Delay {
+    when: Instant,
+}
+
+impl Future for Delay {
+    type Output = &'static str;
+
+    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>)
+            -> Poll<&'static str>
+    {
+        if Instant::now() >= self.when {
+            println!("Hello world");
+            Poll::Ready("done")
+        } else {
+            // 現在のtaskに紐づくwakerを取得する
+            let waker = cx.waker().clone();
+            let when = self.when;
+
+            std::thread::spawn(move || {
+                let now = Instant::now();
+                // まだ実行時間になっていない場合には、実行時間になるまでスレッドをスリープさせる
+                if now < when {
+                    std::thread::sleep(when - now);
+                }
+                // 実行時間になったらそのタスクに紐づくwakerにwakeupするように通知を送る
+                // tokio runtime側でこの通知を受ける処理が必要
+                println!("ssss");
+                waker.wake();
+            });
+
+            Poll::Pending
+        }
     }
 }

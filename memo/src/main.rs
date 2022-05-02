d583bc7233fdd38a5e3f7a5fb0c4d2c90430af17
kinsho-stb <tomoya.kinsho@stanby.inc>
2022-05-03 00:30:15 +0900

    Feat: linkedList

--- memo/src/main.rs
+++ memo/src/main.rs
@@ -1,22 +1,48 @@
+use std::collections::{HashMap, HashSet, LinkedList};
+
+
 fn main() {
 
 
-    let mut vec = Vec::new();
+    // let mut vec = Vec::new();
 
     let first = &[1.0, 2.0];
     let second = &[3.0, 4.0];
 
-    extend(&mut vec, first);
-    extend(&mut vec, second);
+    // extend(&mut vec, first);
+    // extend(&mut vec, second);
 
     // vectorはバッファ足りなくなるとより大きなメモリを確保するために元のバッファを解放し、別アドレスに移動することがある
     // なので↓のようにvectorの参照を引数に渡すと、拡張された場合にはすでに解放されたバッファを差し続けるので
     // コンパイルエラーになってしまう
     // rustでは、このようにデータ競合が起きる可能性をコンパイルで教えてくれる
     // なのでマルチスレッドでのプログラムが簡単にかける3
-    extend(&mut vec, &vec);
+    // extend(&mut vec, &vec);
 
-    println!("{:?}", vec);
+    // println!("{:?}", vec);
+
+
+    // hashMapとHashSet, LinkedListの使い方
+    {
+        let mut hash_set = HashSet::new();
+        let mut hash_map = HashMap::new();
+        // scalaのListとは違って双方向Listなので、前後どちらでもtime complexityはO(1)
+        let mut list = LinkedList::new();
+        let vec = vec!["a", "a", "b", "c", "d", "a", "c"];
+
+        for v in vec {
+            if hash_set.insert(v) {
+                list.push_back(v);
+                hash_map.insert(v, 1);
+            } else {
+                let count = hash_map.get(v).unwrap_or(&1);
+                hash_map.insert(v, *count + 1);
+            }
+        }
+        println!("{:?}", list);
+        println!("{:?}", hash_map);
+
+    }
 
 
 }
